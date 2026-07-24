cd8b0617d645da8b27207cd8ba9a5777111fba23
Adi Shamsian <adishamsian@Adis-MacBook-Air.local>
2026-07-24 20:21:40 +0300

    new func

--- week01/day06_functions_csv.py
+++ week01/day06_functions_csv.py
@@ -67,3 +67,37 @@ def count_employees_from_csv():
         
 result = count_employees_from_csv()
 print(result)
+
+
+# Find all employees older than the given age from CSV file
+def employees_over_age_from_csv(age):
+    employees_over_age_from_csv=[]
+        
+    with open("week01/employees.csv") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            if int(row['age'])> age:
+                 employees_over_age_from_csv.append(row)
+
+        return employees_over_age_from_csv
+
+result = employees_over_age_from_csv(30)
+print(result)
+
+# Find all employee names that start with the given letter
+def employees_name_starts_with_from_csv(letter):
+    employees_name_starts_with=[]
+        
+    with open("week01/employees.csv") as file:
+        reader = csv.DictReader(file)
+
+        for row in reader:
+            name=row["name"]
+            if name[0] == letter:
+                 employees_name_starts_with.append(row["name"])
+
+        return employees_name_starts_with
+
+result = employees_name_starts_with_from_csv("D")
+print(result)
